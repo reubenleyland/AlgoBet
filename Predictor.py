f5e2e54b8ec4e86de3c1af3d3430c8aade80e9d7
Reuben Leyland <112395437+reubenleyland@users.noreply.github.com>
2023-07-10 10:36:54 +0100

    Add files via upload
    Working av goal calulator

--- Predictor.py
+++ Predictor.py
@@ -16,13 +16,10 @@ matches= pd.read_csv("data_21_23.csv")
 total_goals=0
 def calculate_avg_goals_per_game(df, team_name):
     
-    if df[(df['HomeTeam'] == team_name):
-        total_goals =total_goals+ team_matches['FTHG'].sum()
-    elif (df['AwayTeam'] == team_name)]:
-        total_goals = total_goals + team_matches['FTAG'].sum()
-                    
-    team_matches = df[(df['HomeTeam'] == team_name) | (df['AwayTeam'] == team_name)]
-    total_matches = team_matches.shape[0]
+    home_matches = df[(df['HomeTeam'] == team_name)]
+    away_matches=df[(df['AwayTeam'] == team_name)]
+    total_goals = home_matches['FTHG'].sum() + away_matches['FTAG'].sum()
+    total_matches = home_matches.shape[0] +away_matches.shape[0]
     avg_goals_per_game = total_goals / total_matches
     return avg_goals_per_game
 
